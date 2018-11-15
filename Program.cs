85f4fbdf417760f6d0495ef0774b71fe3106a02a
sangeetharavichandran <44968164+sangeetharavichandran@users.noreply.github.com>
2018-11-15 15:44:22 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+           // NewMethod();
+            string[] arr = new string[] { "one", "two" };
+            Console.WriteLine(arr.Length);
+            var res = arr.Where(arr1 => arr1.StartsWith("t"));
+            foreach (var item in res)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("enter email");
+            var e = Console.ReadLine();
+            if(e.Trim().ToLower().Equals("san@san.com"))
+            { Console.WriteLine("valid"); }
+            else
+            { Console.WriteLine("not valid"); }
+        }
+
+        private static void NewMethod()
+        {
             int i, number, fact;
             Console.WriteLine("Enter the Number");
             number = int.Parse(Console.ReadLine());
